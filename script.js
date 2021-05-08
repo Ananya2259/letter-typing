fa3d84f2e476fc60312ab74d3eb3cbf32dbf97b4
Ananya2259 <77260445+Ananya2259@users.noreply.github.com>
2021-05-08 22:13:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,57 @@
-let sound =  document.getElementById("sound")
-let help  =  document.getElementById("help")
+let sound = document.getElementById("sound")
+let help = document.getElementById("help")
 var music = document.getElementById("background-music")
 
-function backFunction(){//Helps in displaying and controlling the back button given in each page.
-    document.getElementById("btn-div").style.display="block"
-    document.getElementById("help-text").style.display="none"
-    document.getElementById("backbtn").style.display="none"   
-    document.getElementById("music-on-off").style.display="none"   
+function backFunction() { //Helps in displaying and controlling the back button given in each page.
+    document.getElementById("btn-div").style.display = "block"
+    document.getElementById("help-text").style.display = "none"
+    document.getElementById("backbtn").style.display = "none"
+    document.getElementById("music-on-off").style.display = "none"
 
 }
-function on(){//Turns on the music.
+
+function on() { //Turns on the music.
     document.getElementById("background-music").play()
 }
-function off(){//Stops the musoc.
+
+function off() { //Stops the musoc.
     document.getElementById("background-music").pause();
 }
 
-function helpText()//Helps in displaying the help page,
+function helpText() //Helps in displaying the help page,
 {
-    document.getElementById("btn-div").style.display="none"  
-    document.getElementById("help-text").style.display="block"
-    document.getElementById("backbtn").style.display="block"
+    document.getElementById("btn-div").style.display = "none"
+    document.getElementById("help-text").style.display = "block"
+    document.getElementById("backbtn").style.display = "block"
 }
-function startMusic(){//Helps in Displaying the sound control page.
-    document.getElementById("btn-div").style.display="none"
-    document.getElementById("music-on-off").style.display="block"
-    document.getElementById("music-on-off").style.color="white"
-    document.getElementById("backbtn").style.display="block"  
+
+function startMusic() { //Helps in Displaying the sound control page.
+    document.getElementById("btn-div").style.display = "none"
+    document.getElementById("music-on-off").style.display = "block"
+    document.getElementById("music-on-off").style.color = "white"
+    document.getElementById("backbtn").style.display = "block"
 }
-function gamestart() {//Helps in displaying the game page.
+
+function gamestart() { //Helps in displaying the game page.
     document.getElementById("homepage").style.display = "none"
     document.getElementById("letter").style.display = "block"
     document.getElementById("time").style.display = "block"
     document.getElementById("points").style.display = "block"
-    
+
     let canvas = document.getElementById('letter');
     let ctx = canvas.getContext('2d');
     let letterBoxSize = 50; //The size of the square inside which a letter will be displayed.
     let yaxis = 0; //Setting the y axis to 0 to make the letterbox drop from the top.
     let yaxis2 = -50; //Setting the y axis2 to -50 to make the 2nd letterbox drop from this axis.
     let yaxis3 = -100; //Setting the y axis3 to 100 to make the 3rd letterbox drop from theis axis.
-    let xaxis = 125;//This is the default xaxix and all the boxes droping in 1st speed will drop in this x axis only.
+    let xaxis = 125; //This is the default xaxix and all the boxes droping in 1st speed will drop in this x axis only.
     let clearingTime; //This is the variable in which the id of the set interval will be stored.
-    let points = 0;//Stores the points.
+    let points = 0; //Stores the points.
     let time = Number(prompt("How many seconds do you want to play -->max:100-->min:10")); //Takes the input from the user on how many seconds the user wants to play.
     let gameTime = time; //Gametime variable is used to show the user the balance time the user has for playing this game.
-    let speed = 10;//The default speed or the first speed in which the game will start.
-    let speedChange = false;//This boolean variable is used to clear the canvas whenever the speed is changing .
-    
+    let speed = 10; //The default speed or the first speed in which the game will start.
+    let speedChange = false; //This boolean variable is used to clear the canvas whenever the speed is changing .
+
 
     if (time >= 10 && time <= 100) { //The minimum and the maximum time a user should play this game is 10 and 100.
         start();
@@ -61,9 +65,9 @@ function gamestart() {//Helps in displaying the game page.
         }
     }
 
-    function start() {//This is the function in which all the gaming functions will be excecuted.
+    function start() { //This is the function in which all the gaming functions will be excecuted.
         droping();
-        let firstgameTime = gameTime;//This variable will help us in calculating the percentage from the time given from the user so that we can increase the  speed according to the percentage.
+        let firstgameTime = gameTime; //This variable will help us in calculating the percentage from the time given from the user so that we can increase the  speed according to the percentage.
         let baltime = setInterval(() => { //This function keeps track of the time which is left for the user to play the game.
             document.getElementById("time").innerHTML = "Balance Time: " + gameTime;
             if (gameTime === 0) { // checks if the game is over
@@ -78,31 +82,31 @@ function gamestart() {//Helps in displaying the game page.
             } else if (gameTime < firstgameTime * 40 / 100) {
                 speed = 1.5;
             }
-            gameTime = gameTime - 1;//Here we minus the gametime by minus one  so that we can show the balance time to the user.
+            gameTime = gameTime - 1; //Here we minus the gametime by minus one  so that we can show the balance time to the user.
         }, 1000);
 
         function droping() {
             clearingTime = setInterval(() => createAndClear(), speed); //Here the box is set to drop in the speed of  the mili second which will be stored in the variable "speed" and also the createandclear function is called.
             document.addEventListener('keydown', function (event) { //The set interval function and eventlistener function will work parallely with no intersection between them.
-                if (speed == 3) {//The eventlistener function mainly focuses on the shooting object.
-                    xaxis = 100;//The xaxis is altered according to the speed in which the boxes will be droping so that the shooting object can shoot the laser correctly.
+                if (speed == 3) { //The eventlistener function mainly focuses on the shooting object.
+                    xaxis = 100; //The xaxis is altered according to the speed in which the boxes will be droping so that the shooting object can shoot the laser correctly.
                 } else if (speed == 1.5) {
                     xaxis = 50;
                 }
 
-                if (event.key === generatedLetter) {//If the speed is the default speed(1st speed)and the user presses the key which is equal to the generated letter
-                    shoot(xaxis, yaxis);//The shoot function is called so that the shooting object will shoot a laser to the xaxis and the yaxis given.
-                    canvasUpdate();//We then call the canvasupdate to clear the laser and increase the point.
-                    nextLetterBox(generatedLetter);//Then we call the nextLetter box function and send the generated letter as paramater so that we can identify which key is peing pressed.
+                if (event.key === generatedLetter) { //If the speed is the default speed(1st speed)and the user presses the key which is equal to the generated letter
+                    shoot(xaxis, yaxis); //The shoot function is called so that the shooting object will shoot a laser to the xaxis and the yaxis given.
+                    canvasUpdate(); //We then call the canvasupdate to clear the laser and increase the point.
+                    nextLetterBox(generatedLetter); //Then we call the nextLetter box function and send the generated letter as paramater so that we can identify which key is peing pressed.
                 }
-                if (speed == 3) {//If the speed is the 2nd speed(3rd speed)and the user presses the key which is equal to the generated letter2
+                if (speed == 3) { //If the speed is the 2nd speed(3rd speed)and the user presses the key which is equal to the generated letter2
                     if (event.key === generatedLetter2) {
-                        shoot(200, yaxis2);//The shoot function is called so that the shooting object will shoot a laser to the xaxis and the yaxis given.
-                        canvasUpdate();//We then call the canvasupdate to clear the laser and increase the point.
-                        nextLetterBox(generatedLetter2);//Then we call the nextLetter box function and send the generated letter as paramater so that we can identify which key is peing pressed.
+                        shoot(200, yaxis2); //The shoot function is called so that the shooting object will shoot a laser to the xaxis and the yaxis given.
+                        canvasUpdate(); //We then call the canvasupdate to clear the laser and increase the point.
+                        nextLetterBox(generatedLetter2); //Then we call the nextLetter box function and send the generated letter as paramater so that we can identify which key is peing pressed.
                     }
                 }
-                if (speed == 1.5) {//If the speed is the last speed(1.5 speed)and the user presses the key which is equal to the generated letter2 and aslo presses another key which is equal to generatedletter 3 
+                if (speed == 1.5) { //If the speed is the last speed(1.5 speed)and the user presses the key which is equal to the generated letter2 and aslo presses another key which is equal to generatedletter 3 
                     //The below to if conditions will call shoot function so that the shooting object will shoot a laser to the xaxis and the yaxis given.
                     //We then call the canvasupdate to clear the laser and increase the point.
                     //Then we call the nextLetter box function and send the generated letter as paramater so that we can identify which key is peing pressed.
@@ -140,7 +144,7 @@ function gamestart() {//Helps in displaying the game page.
                     speedChange = false; //to clear the canvas  once when the speed changes from the  speed 3 to speed 1.5
                 }
             }
-            create();//Create function is called to create the letterbox.
+            create(); //Create function is called to create the letterbox.
             if (yaxis === 350) { //and here I check if the box has touched the bottom of the canvas if yes I again call the nextLetterBox function to drop another box from the top.
                 clear("boundary");
                 nextLetterBox(generatedLetter);
@@ -155,7 +159,7 @@ function gamestart() {//Helps in displaying the game page.
             }
         }
 
-        function clear(param) {//This function recieves a parameter which will either be a boundry or clicked.
+        function clear(param) { //This function recieves a parameter which will either be a boundry or clicked.
             //Boundary means that a letter box has reached the end of the canvas so that we can clear the entire canvas.
             //Clicked means the box has been clicked or pressed so that we can clear only the space which was occupied by the particular letter box.
             if (param == "boundary") {
@@ -182,22 +186,22 @@ function gamestart() {//Helps in displaying the game page.
                 characters.length));
         }
         //Generates three random letters and stores it in three variables
-        generatedLetter = generateLetters(); 
-        generatedLetter2 = generateLetters(); 
-        generatedLetter3 = generateLetters(); 
+        generatedLetter = generateLetters();
+        generatedLetter2 = generateLetters();
+        generatedLetter3 = generateLetters();
 
-        function create() {//Draws the letter box.
+        function create() { //Draws the letter box.
             if (speed > 3) {
-                drawLetterBox(generatedLetter, 125, yaxis, letterBoxSize, "red"); 
+                drawLetterBox(generatedLetter, 125, yaxis, letterBoxSize, "red");
             }
             if (speed == 3) {
-                drawLetterBox(generatedLetter, 100, yaxis, letterBoxSize, "red"); 
-                drawLetterBox(generatedLetter2, 200, yaxis2, letterBoxSize, "red"); 
+                drawLetterBox(generatedLetter, 100, yaxis, letterBoxSize, "red");
+                drawLetterBox(generatedLetter2, 200, yaxis2, letterBoxSize, "red");
             }
             if (speed == 1.5) {
-                drawLetterBox(generatedLetter, 50, yaxis, letterBoxSize, "red"); 
-                drawLetterBox(generatedLetter2, 130, yaxis2, letterBoxSize, "red"); 
-                drawLetterBox(generatedLetter3, 200, yaxis3, letterBoxSize, "red"); 
+                drawLetterBox(generatedLetter, 50, yaxis, letterBoxSize, "red");
+                drawLetterBox(generatedLetter2, 130, yaxis2, letterBoxSize, "red");
+                drawLetterBox(generatedLetter3, 200, yaxis3, letterBoxSize, "red");
             }
 
         }
@@ -215,11 +219,11 @@ function gamestart() {//Helps in displaying the game page.
             ctx.font = "25px Arial" //font size
             ctx.fillText(letter, x + 20, y + 30); //position of the letter inside the box.
 
-            ctx.beginPath();//Helps in drawing a triangle.
+            ctx.beginPath(); //Helps in drawing a triangle.
             ctx.moveTo(145, 420);
             ctx.lineTo(120, 445);
             ctx.lineTo(170, 445);
-            ctx.closePath();//draws the third line
+            ctx.closePath(); //draws the third line
             ctx.strokeStyle = 'black'; //triangle border color
             ctx.stroke(); //triangle border
             // the fill color
@@ -227,15 +231,15 @@ function gamestart() {//Helps in displaying the game page.
             ctx.fill();
         }
 
-        function shoot(endX, endY) {//Shoots a line or laser to the given target.
+        function shoot(endX, endY) { //Shoots a line or laser to the given target.
             ctx.lineWidth = 4;
-            ctx.moveTo(145, 420);//Starting point
-            ctx.lineTo(endX + 20, endY + 50);//This endx+20 and endY+20 are the ending coordinates which will help in shooting the laser correctly to a point where the box is positioned
+            ctx.moveTo(145, 420); //Starting point
+            ctx.lineTo(endX + 20, endY + 50); //This endx+20 and endY+20 are the ending coordinates which will help in shooting the laser correctly to a point where the box is positioned
             ctx.strokeStyle = "black";
             ctx.stroke();
         }
 
-        function canvasUpdate() {//After shooting function this function is called to clear the laser and increase the point.
+        function canvasUpdate() { //After shooting function this function is called to clear the laser and increase the point.
             pointIncrease();
             setTimeout(() => {
                 clearLaser();
@@ -243,22 +247,22 @@ function gamestart() {//Helps in displaying the game page.
             }, 200);
         }
 
-        function clearLaser() {//If I use the clearect directly it was not working so I used it by putting it in a function.
+        function clearLaser() { //If I use the clearect directly it was not working so I used it by putting it in a function.
             ctx.clearRect(0, 0, canvas.width, canvas.height)
         }
 
         function pointIncrease() {
-            points = points + 10;//Increases the point and passes it through the inner html to display.
+            points = points + 10; //Increases the point and passes it through the inner html to display.
             document.getElementById("points").innerHTML = "Points: " + points;
             clear(); //Then to make the box disappear I call the clear function.
         }
 
         function nextLetterBox(letter) { //This function helps to find which letter is being clicked.
             clearInterval(clearingTime); //Stops the set interval.as the id of the set interval will be stored in clearingTime.we stop the setinterval by passing the clearingTime into the clearinterval function.
-          
+
             if (letter == generatedLetter) {
                 yaxis = 0; //To generate the letterbox again from the top.
-                generatedLetter = generateLetters();//Then we again randomly generate letters and store it in the respective variable.
+                generatedLetter = generateLetters(); //Then we again randomly generate letters and store it in the respective variable.
             }
             if (letter == generatedLetter2) {
                 yaxis2 = -50;
@@ -268,7 +272,7 @@ function gamestart() {//Helps in displaying the game page.
                 yaxis3 = -100;
                 generatedLetter3 = generateLetters();
             }
-            droping();// we again call the dropping function.
+            droping(); // we again call the dropping function.
         }
     }
 }
